97be8882ac4aa53e425e2fd97da2bd37c51685ae
Madalina <madalina.zamfira@gmail.com>
2020-11-20 02:51:58 +0100

    add api test with windSpeed parameter

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,13 +10,3 @@ const Home = () => {
 }
 
 export default Home
-
-
-// fetch("https://api.stormglass.io/v1/weather/point?lat=58.5&lng=17.8", {
-//   headers: {
-//     "Authorization": "YOUR API KEY"  // Get your at https://stormglass.io
-//   }
-// }).then(function(response) {
-//   // Do something with response data.
-//   var jsonData = response.json();
-// });
--- src/components/TestApi.js
+++ src/components/TestApi.js
@@ -1,69 +1,44 @@
 import React, { useState, useEffect } from "react";
 
-class TestApi extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: [],
-    };
-  }
+const TestApi = () => {
+  const [windSpeed, setWindSpeed] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
+    const lat = 58.7984;
+    const lng = 17.8081;
+    const params = "windSpeed";
+
     fetch(
-      "https://api.stormglass.io/point?lat=38.678876&lng=-9.340417&params=seaLevel",
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
       {
         headers: {
           Authorization:
-            "0945252a-2abe-11eb-a5a9-0242ac130002-0945271e-2abe-11eb-a5a9-0242ac130002",
+            "aa7e9ff4-2a53-11eb-8ea5-0242ac130002-aa7ea06c-2a53-11eb-8ea5-0242ac130002",
         },
       }
     )
       .then((response) => response.json())
-      .then(function (response) {
-        const hours = response.hours;
-        const minmax = [];
+      .then((jsonData) => Object.entries(jsonData)[0][1])
+      .then((data) => setWindSpeed((prev) => (prev = data[0].windSpeed.sg)));
+  }, []);
+  useEffect(() => {
+    console.log(windSpeed);
+  }, [windSpeed]);
 
-        hours.forEach((hour, index) => {
-          // Skip if first or last element in list.
-          if (index === 0 || index === hours.length - 1) {
-            return;
-          }
-
-          // Get previous, current and next hour.
-          const [previous, current, next] = hours.slice(index - 1, index + 2);
-
-          // Get sea level from first source in response.
-          const {
-            seaLevel: [{ value: currentValue }],
-          } = current;
-          const {
-            seaLevel: [{ value: previousValue }],
-          } = previous;
-          const {
-            seaLevel: [{ value: nextValue }],
-          } = next;
-
-          // Check if current valut is a local minimum.
-          if (previousValue > currentValue < nextValue) {
-            minmax.push([current.time, index, "low"]);
-          }
-
-          // Check if current valut is a local maximum.
-          if (previousValue < currentValue > previousValue) {
-            minmax.push([current.time, index, "high"]);
-          }
-        });
-
-        // Do something with results.
-        console.log(minmax);
-      });
+  function WindAnswer() {
+    if (windSpeed < 10) {
+     return <p>Good to sail</p>;
+    } else if (windSpeed > 10 && windSpeed < 20) {
+     return <p>Risky</p>;
+    } else {
+     return <p>Don't sail!</p>;
+    }
   }
-  render() {
-    return;
-    <div></div>;
-  }
-}
+  return (
+    <div>
+      <WindAnswer />
+    </div>
+  );
+};
 
 export default TestApi;
