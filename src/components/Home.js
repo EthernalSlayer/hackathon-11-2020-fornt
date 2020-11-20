6edd67f6a7ce6161ff989fff46390b912697fc2a
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-11-20 14:28:18 +0100

    add a dynamic render to scrore page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Form, Col, Container, Row , Button} from "react-bootstrap";
+import { Form, Col, Container, Row, Button } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import CityList from "./CityList";
 import cities from "../ressources/cities";
 
-
-const Home = () => {
+const Home = (props) => {
   const [startDate, setStartDate] = useState(new Date());
-  const [arrival,setArrival] = useState();
-  const [departure,setDeparture] = useState();
+  const [arrival, setArrival] = useState("test1");
+  const [departure, setDeparture] = useState("test2");
 
-  function handleChangeDeparture(e){
-    setDeparture(prev => {prev = e.target.value})
-  };
-  function handleChangeArrival(e){
-    setArrival(prev => {prev = e.target.value})
-  };
+  function handleChangeDeparture(e) {
+    let data = e.target.value;
+    setDeparture(data);
+  }
+  function handleChangeArrival(e) {
+    let data = e.target.value;
+    setArrival((prev) => (prev = data));
+  }
   return (
     <div>
       <Container>
@@ -25,16 +26,18 @@ const Home = () => {
           <Form.Group>
             <Form.Row>
               <Form.Label column htmlFor="Departure">
-              Departure
+                Departure
               </Form.Label>
               <Col xs="auto">
-              <Form.Control as="select" onChange={handleChangeDeparture}>
-                  {cities.map(city=>{
-                      return(
-                          <option key={city.name}>{city.name}</option>
-                      )
-                     })}
-              </Form.Control>
+                <Form.Control
+                  as="select"
+                  value={departure}
+                  onChange={handleChangeDeparture}
+                >
+                  {cities.map((city) => {
+                    return <option key={city.name}>{city.name}</option>;
+                  })}
+                </Form.Control>
               </Col>
             </Form.Row>
             <Form.Row>
@@ -42,13 +45,15 @@ const Home = () => {
                 Arrival
               </Form.Label>
               <Col xs="auto">
-              <Form.Control as="select" onChange={handleChangeArrival}>
-                  {cities.map(city=>{
-                      return(
-                          <option key={city.name}>{city.name}</option>
-                      )
-                     })}
-              </Form.Control>
+                <Form.Control
+                  as="select"
+                  value={arrival}
+                  onChange={handleChangeArrival}
+                >
+                  {cities.map((city) => {
+                    return <option key={city.name}>{city.name}</option>;
+                  })}
+                </Form.Control>
               </Col>
             </Form.Row>
             <br />
@@ -61,14 +66,17 @@ const Home = () => {
                 onChange={(date) => setStartDate(date)}
               />
             </Form.Row>
-            
-                <Link to={{pathname:"/Score",
-                            cities:{arrival: arrival,
-                                    departure: departure}
-                        }}>
-                        <Button variant="info" style={{backgroundColor:"black"}}>VALIDER</Button>
-                    </Link>
-    
+
+            <Link
+              to={{
+                pathname: "/Score",
+                cities: { arrival: arrival, departure: departure },
+              }}
+            >
+              <Button variant="info" style={{ backgroundColor: "black" }}>
+                VALIDER
+              </Button>
+            </Link>
           </Form.Group>
         </Row>
       </Container>
